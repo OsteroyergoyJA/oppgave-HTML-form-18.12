286074ee97792f953806fde2c356aa1d127c38b9
OsteroyergoyJA <oosteroyergoy@gmail.com>
2024-12-19 12:23:18 +0100

    Oppgave HTML form 19.12.2024

--- script.js
+++ script.js
@@ -2,22 +2,31 @@ const convertButton = document.querySelector('#convert-button');
 const fromUnit = document.querySelector('#unit');
 const weight = document.querySelector('#weight');
 const output = document.querySelector('#result');
+const output2 = document.querySelector('#input');
+
+
+
 
 convertButton.addEventListener('click', function (event) {
   event.preventDefault();
 
   const fromWeight = Number(weight.value);
-
+  
   if (fromUnit.value === 'kilogram-to-pounds') {
 
-   
-    output.textContent = fromWeight * 2.2046;
+    output.textContent = fromWeight * 2.2046 + " pounds";
+    output2.textContent = fromWeight + " kilogram";
 
   } else if (fromUnit.value === 'pound-to-kilogram') {
 
-    output.textContent = fromWeight / 2.2046
+    output.textContent = fromWeight / 2.2046 + " kilogram";
+    output2.textContent = fromWeight + " pounds";
 
   } else {
     console.log('no unit selected');
   }
+
+
 });
+
+/* Kunne tenkt meg en måte å resette animasjonen på for hvert klikk*/
