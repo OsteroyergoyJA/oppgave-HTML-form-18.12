357ed34075fb85435f242d57295cfe8530039718
OsteroyergoyJA <oosteroyergoy@gmail.com>
2024-12-19 13:23:21 +0100

    innlevering javakalkulator 19.12.2024

--- script.js
+++ script.js
@@ -12,22 +12,21 @@ convertButton.addEventListener('click', function (event) {
 
   const fromWeight = Number(weight.value);
   
-  
   if (fromUnit.value === 'kilogram-to-pounds') {
-
+    
     output.textContent = fromWeight * 2.2046 + " pounds";
     output2.textContent = fromWeight + " kilogram";
-
+    
   } else if (fromUnit.value === 'pound-to-kilogram') {
-
+    
     output.textContent = fromWeight / 2.2046 + " kilogram";
     output2.textContent = fromWeight + " pounds";
-
+    
   } else {
     console.log('no unit selected');
   }
-
-
+  
+  
 });
 
 /* Kunne tenkt meg en måte å resette animasjonen på for hvert klikk*/
